d061727d030464a01ec2ea1244731ab5270c39f7
lcs-tdauletov <32198299+lcs-tdauletov@users.noreply.github.com>
2018-05-10 09:59:17 -0400

    Gordon was done.

--- EnglishOrFrench/main.swift
+++ EnglishOrFrench/main.swift
@@ -1,18 +1,17 @@
 import Foundation
 
 
-var countTheSs = 0
-var countTheTs = 0
-
+// INPUT
+var expectedLines = 0
 while true {
     
     print("How many lines")
     
-    guard let expectedLines = readLine() else {
+    guard let someString = readLine() else {
         continue
     }
     
-    guard let intExpectedLines = Int(expectedLines) else  {
+    guard let intExpectedLines = Int(someString) else  {
         continue
     }
     
@@ -20,34 +19,42 @@ while true {
         continue
     }
     
+    // All tests passed, so assign to global variable
+    expectedLines = intExpectedLines
+    break
     
-for _ in 1...intExpectedLines {
-  
+}
+
+// PROCESS
+var frenchCharacterCount = 0
+var englishCharacterCount  = 0
+for _ in 1...expectedLines {
+    
     guard let givenLine = readLine() else {
-       
+        
         continue
     }
     for character in givenLine {
         switch character {
-            case "S", "s":
-            countTheSs += 1
-            case "T", "t" :
-            countTheTs += 1
-            default:
+        case "S", "s":
+            frenchCharacterCount += 1
+        case "T", "t" :
+            englishCharacterCount  += 1
+        default:
             continue
         }
         
     }
 }
-    if countTheTs == countTheSs || countTheTs < countTheSs {
-        print("Probably, it's French text")
-        exit(9)
-    } else {
-        print("Probably, it's English text")
-        exit(9)
-    }
-    
-}
 
+// OUTPUT
+
+if englishCharacterCount  == frenchCharacterCount || englishCharacterCount  < frenchCharacterCount {
+    print("Probably, it's French text")
+    exit(9)
+} else {
+    print("Probably, it's English text")
+    exit(9)
+}
 
 
